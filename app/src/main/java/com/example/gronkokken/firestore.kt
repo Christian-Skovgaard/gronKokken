fe9282072a730d8d080a28a202eaa672c21fdeec
Christian-Skovgaard <cellochr@gmail.com>
2025-05-15 10:17:36 +0200

    updated file name hotfix

--- app/src/main/java/com/example/gronkokken/firestore.kt
+++ app/src/main/java/com/example/gronkokken/firestore.kt
@@ -3,7 +3,8 @@ package com.example.gronkokken
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
 
-class firestore {
+class Firestore {
+    //connection detaljer kan findes i app/google-services.json
     val db = Firebase.firestore
 
 }
