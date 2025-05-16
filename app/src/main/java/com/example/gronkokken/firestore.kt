ce8e38b70982df395a9af0133bddaceef650fa05
Christian-Skovgaard <cellochr@gmail.com>
2025-05-16 09:25:40 +0200

    Hotfix to be able to merge

--- app/src/main/java/com/example/gronkokken/firestore.kt
+++ app/src/main/java/com/example/gronkokken/firestore.kt
@@ -3,7 +3,6 @@ package com.example.gronkokken
 import android.util.Log
 import com.example.gronkokken.dataclasses.Recipe
 import com.google.firebase.Firebase
-import com.google.firebase.R
 import com.google.firebase.firestore.firestore
 import com.google.firebase.firestore.toObject
 
@@ -28,7 +27,7 @@ class Firestore {
             Log.d("DB-call","lol, did not work")
         }
 
-        return returnList.toList()  //den returnere en tom liste, også hvis noget går galt, det tror jeg er fedest
+        return returnList.toList()  //den returnere en tom liste, også hvis noget går galt, det tror jeg er fedest.
         //Hvis vi har tid kan vi få appen til at skrive en fejlbesked i ui hvis den giver null fx.
     }
 }
