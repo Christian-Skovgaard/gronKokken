6f09e3c0cf162617edb5c89036321248686eb033
Sahra <ali.sarah.w@icloud.com>
2025-05-20 10:58:05 +0200

    comit

--- app/src/main/java/Frontpage.kt
+++ app/src/main/java/Frontpage.kt
@@ -5,21 +5,8 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -29,16 +16,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role.Companion.Image
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.gronkokken.ui.theme.GronKokkenTheme
-import com.example.gronkokken.ui.theme.Typography
 
 class FrontPage : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,114 +35,189 @@ class FrontPage : ComponentActivity() {
         }
     }
 }
+
 @Composable
 fun FrontPageScreen(modifier: Modifier = Modifier) {
-    val week by remember {mutableStateOf("UGENS OPSKRIFT")}
-    val season by remember {mutableStateOf("RÅVARER I SÆSON")}
-    val recipes by remember { mutableStateOf("OPSKRIFTER")}
-    val  clima by remember { mutableStateOf("VORES KLIMAPLAN")}
-    val pics by remember { mutableStateOf("BILLEDER")}
-    val overview by remember { mutableStateOf("OVERVIEW")}
-    val BalooFontFamily = FontFamily(
-        Font(R.font.baloo2))
-//logo
-    Column{
+    val week by remember { mutableStateOf("UGENS OPSKRIFT") }
+    val season by remember { mutableStateOf("RÅVARER I SÆSON") }
+    val recipes by remember { mutableStateOf("OPSKRIFTER") }
+    val clima by remember { mutableStateOf("VORES KLIMAPLAN") }
+    val pics by remember { mutableStateOf("BILLEDER") }
+    val robotoBold = FontFamily(Font(R.font.roboto))
+
+
+    Column {
         Image(
-            painter = painterResource(id = R.drawable.logo2), // brug dit billednavn her
+            painter = painterResource(id = R.drawable.logo2),
             contentDescription = "App logo",
             modifier = Modifier
                 .padding(top = 16.dp)
-                .width(450.dp).height(200.dp), // justér størrelsen her
+                .width(450.dp)
+                .height(200.dp),
             contentScale = ContentScale.Fit
         )
 
-    Column(
-        modifier = modifier
-            .padding(16.dp)
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(20.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-
-
-        Spacer(modifier = Modifier.width(16.dp))
-        // Ugens Opskrift
-        Button(
-            onClick = { Log.d("BTN", "Ugens opskrift klik") },
-            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF69BFFF)),
-            modifier = Modifier.fillMaxWidth()
-                .height(80.dp),        ) {
-            Text(text = "👨‍🍳 $week",
-                    color = Color.Black)
-
-        }
-        Spacer(modifier = Modifier.width(16.dp))
-        // Råvarer i sæson
-        Button(
-            onClick = { Log.d("BTN", "Råvarer i sæson klik") },
-            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFFBA27)),
-            modifier = Modifier.fillMaxWidth()
-                .height(80.dp),    ) {
-            Text(text = "🌽 $season", color = Color.Black)
-        }
-        Spacer(modifier = Modifier.width(16.dp))
-        // Opskrifter
-        Button(
-            onClick = { Log.d("BTN", "Opskrifter klik") },
-            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFCEBAFF)),
-            modifier = Modifier.fillMaxWidth()
-                .height(80.dp),        ) {
-            Text(text = "📖 $recipes", color = Color.Black)
-        }
-        Spacer(modifier = Modifier.width(16.dp))
-        // Vores klimaplan
-        Button(
-            onClick = { Log.d("BTN", "Klimaplan klik") },
-            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF4B84AD)),
-            modifier = Modifier.fillMaxWidth()
-                .height(80.dp),)
-        {
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Image(
-                    painter = painterResource(id = R.drawable.corn), // ← dit ikon her
-                    contentDescription = "Billede ikon",
-                    modifier = Modifier
-                        .size(24.dp)
-                        .padding(end = 8.dp)
-                )
-
-                Text(text = " $clima", color = Color.Black)
-            }}
-        Spacer(modifier = Modifier.width(16.dp))
-        // Billeder
-
-        Button(
-            onClick = { Log.d("BTN", "Billeder klik") },
-            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFF8380)),
-            modifier = Modifier
-                .weight(1f)
-                .height(80.dp),
+        Column(
+            modifier = modifier
+                .padding(16.dp)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(30.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Image(
-                    painter = painterResource(id = R.drawable.chefs), // ← dit ikon her
-                    contentDescription = "Billede ikon",
-                    modifier = Modifier
-                        .size(24.dp)
-                        .padding(end = 8.dp)
-                )
-                Text(text = " $pics", color = Color.Black)
+
+            Button(
+                onClick = { Log.d("BTN", "Ugens opskrift klik") },
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF69BFFF)),
+                modifier = Modifier.fillMaxWidth().height(80.dp)
+            ) {
+                Box(
+                    modifier = Modifier.fillMaxWidth(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    // Billedet placeres helt til venstre
+                    Image(
+                        painter = painterResource(id = R.drawable.chefhat),
+                        contentDescription = "ikon",
+                        modifier = Modifier
+                            .size(50.dp)
+                            .align(Alignment.CenterStart) // venstrestillet
+                            .padding(start = 16.dp)
+                    )
+
+                    // Teksten centreres
+                    Text(
+                        text = week,
+                        fontSize = 25.sp,
+                        fontFamily = robotoBold,
+                        color = Color.Black
+                    )
+                }
+            }
+
+            Button(
+                onClick = { Log.d("BTN", "Råvarer i sæson klik") },
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFFBA27)),
+                modifier = Modifier.fillMaxWidth().height(80.dp)
+            ) {
+                Box(
+                    modifier = Modifier.fillMaxWidth(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    // Billedet placeres helt til venstre
+                    Image(
+                        painter = painterResource(id = R.drawable.corn),
+                        contentDescription = "Billeder ikon",
+                        modifier = Modifier
+                            .size(50.dp)
+                            .align(Alignment.CenterStart) // venstrestillet
+                            .padding(start = 16.dp)
+                    )
+
+                    // Teksten centreres
+                    Text(
+                        text = season,
+                        fontSize = 25.sp,
+                        fontFamily = robotoBold,
+                        color = Color.Black
+                    )
+                }
+            }
+
+            Button(
+                onClick = { Log.d("BTN", "Opskrifter klik") },
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFCEBAFF)),
+                modifier = Modifier.fillMaxWidth().height(80.dp)
+            ) {
+                Box(
+                    modifier = Modifier.fillMaxWidth(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    // Billedet placeres helt til venstre
+                    Image(
+                        painter = painterResource(id = R.drawable.menu),
+                        contentDescription = "ikon",
+                        modifier = Modifier
+                            .size(50.dp)
+                            .align(Alignment.CenterStart) // venstrestillet
+                            .padding(start = 16.dp)
+                    )
+
+                    // Teksten centreres
+                    Text(
+                        text = recipes,
+                        fontSize = 25.sp,
+                        fontFamily = robotoBold,
+                        color = Color.Black
+                    )
+                }
+            }
+
+
+            Button(
+                onClick = { Log.d("BTN", "Klimaplan klik") },
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF4B84AD)),
+                modifier = Modifier.fillMaxWidth().height(80.dp)
+            ) {
+                Box(
+                    modifier = Modifier.fillMaxWidth(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    // Billedet placeres helt til venstre
+                    Image(
+                        painter = painterResource(id = R.drawable.clima),
+                        contentDescription = "Billeder ikon",
+                        modifier = Modifier
+                            .size(40.dp)
+                            .align(Alignment.CenterStart) // venstrestillet
+                            .padding(start = 8.dp)
+                    )
+
+                    // Teksten centreres
+                    Text(
+                        text = clima,
+                        fontSize = 25.sp,
+                        fontFamily = robotoBold,
+                        color = Color.Black
+                    )
+                }
+            }
+
+            Button(
+                onClick = { Log.d("BTN", "Billeder klik") },
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFF8380)),
+                modifier = Modifier.fillMaxWidth().height(80.dp)
+            ) {
+                Box(
+                    modifier = Modifier.fillMaxWidth(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    // Billedet placeres helt til venstre
+                    Image(
+                        painter = painterResource(id = R.drawable.pics),
+                        contentDescription = "Billeder ikon",
+                        modifier = Modifier
+                            .size(50.dp)
+                            .align(Alignment.CenterStart) // venstrestillet
+                            .padding(start = 16.dp)
+                    )
+
+                    // Teksten centreres
+                    Text(
+                        text = pics,
+                        fontSize = 25.sp,
+                        fontFamily = robotoBold,
+                        color = Color.Black
+                    )
+                }
             }
         }
-
     }
-    }}
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     GronKokkenTheme {
-        FrontPageScreen(
-
-        )
+        FrontPageScreen()
     }
 }
