f9511065ff4959478da6eef85f67d3f42a65e2dd
bananpalme <nigginz12@gmail.com>
2025-05-22 01:07:31 +0200

    made button composable and added them to the landingpage

--- app/src/main/java/com/example/gronkokken/landingpage/Landingpage.kt
+++ app/src/main/java/com/example/gronkokken/landingpage/Landingpage.kt
@@ -1,5 +1,6 @@
 package com.example.gronkokken.landingpage
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
@@ -7,10 +8,13 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -22,6 +26,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -70,6 +75,15 @@ fun Landingpage() {
                 .padding(vertical = 6.dp, horizontal = 16.dp)
         )
 
+        LandingButton("Færdig", 166, 48, 0xFF121212, 25)
+
+        Spacer(modifier = Modifier.height(80.dp))
+
+        LandingButton("Lærer", 134, 40, 0xFF9A9A9A, 20)
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        LandingButton("Gæst?", 134, 40, 0xFF9A9A9A, 20)
 
 
     }
