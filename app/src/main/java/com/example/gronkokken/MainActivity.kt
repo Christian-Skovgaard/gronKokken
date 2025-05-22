a5b1dc72cde04c46cbca5235551e0d4ed95fe17b
Sahra <ali.sarah.w@icloud.com>
2025-05-22 13:03:08 +0200

    comit

--- app/src/main/java/com/example/gronkokken/MainActivity.kt
+++ app/src/main/java/com/example/gronkokken/MainActivity.kt
@@ -5,17 +5,32 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
+import com.example.gronkokken.com.example.gronkokken.ui.theme.FrontPageTeacherScreen
 import com.example.gronkokken.components.Navigation
 import com.example.gronkokken.components.UserViewModel
 import com.example.gronkokken.recipeListScreen.RecipeListScreen
@@ -30,24 +45,28 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            FrontPageTest()
             val userViewModel: UserViewModel = viewModel()
             val navController = rememberNavController()
-            Navigation(navController)
 
-            GronKokkenTheme {
-            }
+
+
         }
     }
 }
-
-
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun FrontPageTest (userViewModel: UserViewModel = viewModel()) {
+    val role = userViewModel.role // fx: "lærer" eller "elev"
+    Text("hey")
+
+    val isTeacher = false
+
+    if (isTeacher) {
+        // Viser lærerens knapper
+        FrontPageTeacherScreen()
+    } else {
+        FrontPageScreen()
+    }
 }
 
 @Preview(showBackground = true)
