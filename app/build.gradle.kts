790fc21cd58d64060948b771cd035ac9b1cd1a51
nixhajete09 <nixhajete05@gmail.com>
2025-05-29 22:04:40 +0200

    init commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.firebase.firestore.ktx)
+    implementation(libs.play.services.maps)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -61,4 +62,7 @@ dependencies {
     //firebase dependencies
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.analytics)    //vi bruger ikke analytics til noget
+    implementation("nl.dionsegijn:konfetti-compose:2.0.3")
+    implementation("nl.dionsegijn:konfetti-core:2.0.3")
+
 }
--- app/src/main/java/com/example/gronkokken/MainActivity.kt
+++ app/src/main/java/com/example/gronkokken/MainActivity.kt
@@ -1,52 +1,20 @@
 package com.example.gronkokken
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.gronkokken.ui.theme.GronKokkenTheme
-import com.google.firebase.Firebase
-import com.google.firebase.firestore.firestore
-import kotlinx.coroutines.tasks.await
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            GronKokkenTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "me",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            UploadScreen()
         }
-    }
-}
-
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GronKokkenTheme {
-        Greeting("Android")
     }
 }
